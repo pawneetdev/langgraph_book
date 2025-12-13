843ba699a8ee060a028768ef717bd9928f0359b8
pawneetdev <rubalps@gmail.com>
2025-12-13 16:19:47 +0530

    Conversation history dumped

--- memory_agent.py
+++ memory_agent.py
@@ -38,3 +38,15 @@ while user_input != "exit":
     conversation_history = result["messages"]
     user_input = input("\nEnter: ")
 
+with open("logging.txt", "w") as file:
+    file.write("Your conversation log:\n\n")
+
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            file.write(f"You: {message.content}\n")
+        elif isinstance(message, AIMessage):
+            file.write(f"AI: {message.content}\n\n")
+
+    file.write("End of conversation")
+
+print("Conversation saved to logging.txt")
